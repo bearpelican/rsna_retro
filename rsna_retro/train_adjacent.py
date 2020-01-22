b8a0321238f36c0f3235ca90a37a63f559bac723
Andrew Shaw <andrew.t.shaw@gmail.com>
2020-01-22 13:39:36 -0800

    Training with adjacent

--- rsna_retro/train_adjacent.py
+++ rsna_retro/train_adjacent.py
@@ -11,11 +11,12 @@ from .train3d import *
 
 # Cell
 class TfmSlice:
-    def __init__(self, df, path, c3=False):
+    def __init__(self, df, path, windowed=False, num_adj=1):
         self.fn = get_pil_fn(path)
         self.tt = ToTensor()
         self.df = df
-        self.c3 = c3
+        self.windowed = windowed
+        self.num_adj = num_adj
 
     def get_adj(self, idx, x_mid, sid_mid):
         if idx < 0 or idx >= self.df.shape[0] \
@@ -28,31 +29,34 @@ class TfmSlice:
         idx = self.df.index.get_loc(item)
         sid = self.df.loc[item].SeriesInstanceUID
         x = self.tt(self.fn(item))
-        x_prev = self.get_adj(idx-1, x, sid)
-        x_next = self.get_adj(idx+1, x, sid)
-        x = x[:1] if self.c3 else x
+        x_prevs, x_nexts = [], []
+        for i in range(1, self.num_adj+1):
+            x_prevs.append(self.get_adj(idx-i, x, sid)[:1])
+            x_nexts.append(self.get_adj(idx+i, x, sid)[:1])
+        x = x if self.windowed else x[:1]
 
-        return TensorCTScan(torch.cat([x_prev[:1], x, x_next[:1]]))
+        return TensorCTScan(torch.cat([*x_prevs, x, *x_nexts]))
 
 # Cell
 mean_5c = [mean[0], *mean, mean[0]]
 std_5c = [std[0], *std, std[0]]
 
 mean_adj = [mean[0]]*3
-std_adj = [mean[0]]*3
+std_adj = [std[0]]*3
 
 # Cell
-def get_adj_data(bs, sz, splits, img_dir=path_jpg256, c3=False, df=Meta.df_comb, test=False):
-    tfm = TfmSlice(df, img_dir, c3=c3)
-    mean,std = (mean_adj, std_adj) if c3 else (mean_5c, std_5c)
+def get_adj_data(bs, sz, splits, img_dir=path_jpg256, windowed=False, num_adj=1, df=Meta.df_comb, test=False):
+    tfm = TfmSlice(df, img_dir, windowed=windowed, num_adj=num_adj)
+    num_c = (1+2*num_adj)
+    mean,std = ([mean[0]]*num_c, [std[0]]*num_c) if windowed else (mean_5c, std_5c)
     return get_data_gen(L(list(df.index)), bs=bs, img_tfm=tfm, sz=sz, splits=splits,
                        mean=mean, std=std, test=test)
 
 # Cell
-def get_adj_test_data(bs=512, sz=256, tst_dir='tst_jpg', c3=False):
+def get_adj_test_data(bs=512, sz=256, tst_dir='tst_jpg', windowed=False):
     tst_fns = Meta.df_tst.index.values
     tst_splits = [L.range(tst_fns), L.range(tst_fns)]
-    tst_dbch = get_adj_data(bs, sz, tst_splits, path/tst_dir, df=Meta.df_tst, c3=c3, test=True)
+    tst_dbch = get_adj_data(bs, sz, tst_splits, path/tst_dir, df=Meta.df_tst, windowed=windowed, test=True)
 #     tst_dbch = get_data_gen(tst_fns, bs=bs, img_tfm=get_pil_fn(path/tst_dir), sz=sz, splits=tst_splits, test=True)
     tst_dbch.c = 6
     return tst_dbch
